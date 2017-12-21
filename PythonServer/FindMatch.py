2b356336e75aa47663b1bb0a5e5aade46d3a4f2a
robertkajnak <hezirisz@gmail.com>
2017-12-21 18:15:02 +0100

    added more personality stuff

--- PythonServer/FindMatch.py
+++ PythonServer/FindMatch.py
@@ -8,7 +8,7 @@ import requests
 
 
 language_translator = LanguageTranslator(username='53d06608-1e59-4fd3-b7a6-06272abf99a9', password='HnXHP3R8pgDj')
-personality_insights = PersonalityInsights(username='df9417a6-0efd-4232-9f1c-3b8b265c3830', password='ZFkRifBZ0SBj')
+personality_insights = PersonalityInsights(username='df9417a6-0efd-4232-9f1c-3b8b265c3830', password='ZFkRifBZ0SBj', version='2017-12-4')
 
 
 class FindMatch:
@@ -21,7 +21,7 @@ class FindMatch:
         # create database if none is present
         if not os.path.isfile('database.csv'):
             with open('test.txt') as test:
-                test_profile = personality_insights.profile(test.read())
+                test_profile = personality_insights.profile(test.read(), content_type='text/plain')
                 self.create_database(test_profile)
 
         self.database = pd.read_csv('database.csv')
@@ -32,17 +32,19 @@ class FindMatch:
         token = data['token']
         r = requests.get("https://graph.facebook.com/v2.11/me/posts?access_token=" + token)
         fb_data = json.loads(r.content)
+
         self.text = ''
         for i in range(len(fb_data['data'])):
             self.text += fb_data['data'][i]['story']
-
+            
+        self.text = 'For any system to be considered cognitive there are three basic requirements that need to be fulfilled. First of all, the system ought to expand human cognition. With expanding human cognition, we mean it should enhance human abilities either qualitatively or quantitatively. Qualitatively enhanced human cognition would be the case when a system is better at some task than a human ever could be. For instance, meteorologists use predictive models devised by computers to forecast the weather, because merely looking at the sky has not always proven to be too fruitful. However, for our system the latter type of enhancement is aimed for: quantitatively enhanced human cognition. Manually mining Facebook for profile information and timeline posts of every system user would be a daunting, if not impossible, task for any human. In contrary, a program can devise information from multiple profiles in mere seconds. In this way, the system will enhance human cognition. '
         self.user_characteristics = [data['usertype']]
         self.date = data['date']
-        self.preferences = list(data['likes'])
+        self.preferences = json.loads(data['likes'])
 
         # parameters to be tuned
-        self.gender_correction = 0.8
-        self.age_correction = 1.01
+        # self.gender_correction = 0.8
+        # self.age_correction = 1.01
 
         # provide user with sliders if word count is below 600
         # if len(self.text.split()) < 600:
@@ -52,6 +54,15 @@ class FindMatch:
         # read what weight vector is currently being used
         self.weight_vector_id = 0
         self.weight_vector = np.load('weight_vectors.npy')[self.weight_vector_id]
+
+
+    def check_ascii(self, s):
+        try:
+            s.decode('ascii')
+        except UnicodeDecodeError:
+            return False
+        else:
+            return True
 
 
     def create_database(self, generic_profile):
@@ -98,7 +109,7 @@ class FindMatch:
 
 
     def analyze_personality(self):
-        profile = personality_insights.profile(self.text)
+        profile = personality_insights.profile(self.text, content_type='text/plain')
         return profile
 
 
@@ -130,28 +141,27 @@ class FindMatch:
 
         for i in range(self.index_number):
 
-            # Now it only checks if user_type is not the same. Must also check if dates are the same:
-            # if self.database['date'].loc[self.index_number] == self.database['date'].loc[i]:
+            if self.database['date'].loc[self.index_number] == self.database['date'].loc[i]:
 
-            if self.database['user_type'].loc[self.index_number] != self.database['user_type'].loc[i]:
-                # calculate error
-                error = self.database.loc[i][:22] - self.database.loc[self.index_number][:22]
-                weighted_error = np.array(error) * self.weight_vector
-                error = np.sum(np.absolute(weighted_error))
+                if self.database['user_type'].loc[self.index_number] != self.database['user_type'].loc[i]:
+                    # calculate error
+                    error = self.database.loc[i][:22] - self.database.loc[self.index_number][:22]
+                    weighted_error = np.array(error) * self.weight_vector
+                    error = np.sum(np.absolute(weighted_error))
 
-                # change error depending on gender and age
-                if self.database['gender'].loc[i] == self.database['gender'].loc[self.index_number]:
-                    error *= self.gender_correction
-                if self.database['age'].loc[i] != self.database['age'].loc[self.index_number]:
-                    error *= self.age_correction * (self.database['gender'].loc[i] - self.database['gender'].loc[self.index_number])
+                    # change error depending on gender and age
+                    # if self.database['gender'].loc[i] == self.database['gender'].loc[self.index_number]:
+                    #     error *= self.gender_correction
+                    # if self.database['age'].loc[i] != self.database['age'].loc[self.index_number]:
+                    #     error *= self.age_correction * (self.database['gender'].loc[i] - self.database['gender'].loc[self.index_number])
 
-                if i == 0:
-                    current_best = error
-                    match_id = i
+                    if i == 0:
+                        current_best = error
+                        match_id = i
 
-                if error < current_best:
-                    current_best = error
-                    match_id = i
+                    if error < current_best:
+                        current_best = error
+                        match_id = i
 
         return match_id
 
@@ -164,4 +174,9 @@ class FindMatch:
         match_id = self.find_match()
 
         return match_id
+
+
+data = '{"token":"EAAXckKsm7pQBACnaXqAFUb7UCoEuR8IvSobbyZBKRBcsqW0ZB7tvOJxcPymtXLZAh1Vk0S1qeCXx1XZCXcrZBTJfieReYB9IWNebWmZBY5p5GzvhqBHW3YMGsiAYhhHSy4imZA4R9CtOfgqbKgBd6y1MRF7V4ZALMRoa31H12CJztoKZCJZAlutMEsBX1x7HHItak3Svn7aj1s5gZDZD","usertype":0,"date":"2017-12-19","likes":"[1, 0, 0, 0, 0]"}'
+fm = FindMatch(data)
+print fm.main()
 
---
+++ PythonServer/Server_2.py
@@ -0,0 +1,39 @@
+import socket
+import FindMatch as fm
+import Evolution as ev
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip = socket.gethostbyname(socket.gethostname())
+port = 23260
+
+address = (ip, port)
+print address
+
+server.bind(address)
+server.listen(1)
+print "[+] Started listening on ", ip, ":", port
+
+client, addr = server.accept()
+print "[+] Got a connection from ", addr[0], ":", addr[1]
+
+while True:
+    data = client.recv(1024)
+    print "[+] Received ", data, " from the client"
+    print "     Processing data"
+
+    fm = fm.FindMatch(data)
+    match = fm.main()
+    print match
+
+    if data == "disconnect":
+        client.send("Closing client")
+        client.close()
+        break
+
+    else:
+        client.send(match)
+        print "     Processing done\n[+] Reply sent"
+
+
+
+
