7996afd64e6b480786d27c3a6a9fda61082131ea
robertkajnak <hezirisz@gmail.com>
2017-12-21 02:49:42 +0100

    changed around the dbless

--- PythonServer/Server_nodb.py
+++ PythonServer/Server_nodb.py
@@ -20,16 +20,28 @@ client, addr = server.accept()
 print "[+] Got a connection from ", addr[0], ":", addr[1]
 
 while True:
+    f = open('log.txt', 'w')
     data = client.recv(1024)
+    f.write('data as recieved:\n')
+    f.write(data)
     #print "[+] Received ", data, " from the client"
     decr = json.loads(data)
+    f.write('\n\nJSON deserialized(py object form):\n')
+    f.write(decr.__str__)
+    
+    f.write('\n\nToken only:')
+    f.write(decr['token'])
     print decr['token']
     print "     Processing data"
     
     r = requests.get("https://graph.facebook.com/v2.11/me/posts?access_token=" + decr['token'])
 
+    f.write('\n\nResponse to the request:\n')
+    f.write(r.__str__)
     print r.content
     
+    f.write('\n\nActual request content:\n' )
+    f.write(r.content)
     dataToDbObject = json.loads(r.content)
 
     if data != " ":
@@ -41,7 +53,7 @@ while True:
         print "Client sent ", data
         print "     Processing done\n[+] Reply sent"
 
-
+    f.close()
 ''' sample token:
     EAAXckKsm7pQBACnaXqAFUb7UCoEuR8IvSobbyZBKRBcsqW0ZB7tvOJxcPymtXLZAh1Vk0S1qeCXx1XZCXcrZBTJfieReYB9IWNebWmZBY5p5GzvhqBHW3YMGsiAYhhHSy4imZA4R9CtOfgqbKgBd6y1MRF7V4ZALMRoa31H12CJztoKZCJZAlutMEsBX1x7HHItak3Svn7aj1s5gZDZD
 '''
